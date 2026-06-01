80b7f0c572338ce7267f75ddfbea5d595d9e761d
LilBoooopp <cbopp@student.42lausanne.ch>
2026-06-01 16:13:50 +0200

    optimised .c level2

--- reverseme/Level2/level2.c
+++ reverseme/Level2/level2.c
@@ -3,38 +3,27 @@
 #include <stdlib.h>
 
 int main(void) {
-    char *password = "delabere";
+    const char *password = "delabere";
     char tmp[24];
+    char str[9];
+    int idx;
+    int pos;
+
     printf("Please enter key: ");
-    if (scanf("%23s", tmp) == 1){
-        if (tmp[1] == '0')
-        {
-            if (tmp[0] == '0')
-            {
-                fflush(stdin);
-                char str[9];
-                memset(str, 0, 9);
-                str[0] = 'd';
-                int idx = 2;
-                char letter[4];
-                while (strlen(str) < 8 && idx < strlen(tmp))
-                {
-                    int i = 0;
-                    while (i < 3) {
-                        letter[i] = tmp[idx];
-                        i++;
-                        idx++;
-                    }
-                    letter[3] = '\0';
-                    str[strlen(str)] = atoi(letter);
-                }
-                if (strcmp(password, str) == 0) {
-                    printf("Good job.\n");
-                    return (0);
-                }
-            }
-        }
+    if (scanf("%23s", tmp) != 1 || tmp[1] != '0' || tmp[0] != '0'){
+        printf("Nope.\n");
+        return (0);
     }
-    printf("Nope.\n");
+    fflush(stdin);
+    memset(str, 0, sizeof(str));
+    str[0] = 'd';
+    idx = 2;
+    pos = 1;
+    while (pos < 8 && idx + 3 <= (int)strlen(tmp))
+    {
+        str[pos++] = atoi((char[4]){ tmp[idx], tmp[idx+1], tmp[idx+2], '\0'});
+        idx += 3;
+    }
+    printf("%s\n", strcmp(password, str) ? "Nope." : "Good job.");
     return (0);
 }
