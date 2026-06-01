6c8a55328b59417434286f1eee203a77bf80de3d
LilBoooopp <cbopp@student.42lausanne.ch>
2026-06-01 16:02:50 +0200

    updated code

--- reverseme/Level2/level2.c
+++ reverseme/Level2/level2.c
@@ -6,7 +6,7 @@ int main(void) {
     char *password = "delabere";
     char tmp[24];
     printf("Please enter key: ");
-    if (scanf("%23s", tmp) > 0){
+    if (scanf("%23s", tmp) == 1){
         if (tmp[1] == '0')
         {
             if (tmp[0] == '0')
@@ -17,7 +17,7 @@ int main(void) {
                 str[0] = 'd';
                 int idx = 2;
                 char letter[4];
-                while (strlen(str) < 8 && idx <= 23)
+                while (strlen(str) < 8 && idx < strlen(tmp))
                 {
                     int i = 0;
                     while (i < 3) {
@@ -27,10 +27,10 @@ int main(void) {
                     }
                     letter[3] = '\0';
                     str[strlen(str)] = atoi(letter);
-                    if (strcmp(password, str) == 0) {
-                        printf("Good job.\n");
-                        return (0);
-                    }
+                }
+                if (strcmp(password, str) == 0) {
+                    printf("Good job.\n");
+                    return (0);
                 }
             }
         }
